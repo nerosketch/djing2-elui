efbd473b1f29718227fa1cab43d8aea365d687d9
Dmitry Novikov <nerosketch@gmail.com>
2020-12-23 13:40:16 +0300

    cookie expires in one day

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -3,10 +3,14 @@ import Cookies from 'js-cookie'
 // App
 const sidebarStatusKey = 'sidebar_status'
 export const getSidebarStatus = () => Cookies.get(sidebarStatusKey)
-export const setSidebarStatus = (sidebarStatus: string) => Cookies.set(sidebarStatusKey, sidebarStatus)
+export const setSidebarStatus = (sidebarStatus: string) => Cookies.set(sidebarStatusKey, sidebarStatus, {
+  expires: 1
+})
 
 // User
 const tokenKey = 'djing2_admin_access_token'
 export const getToken = () => Cookies.get(tokenKey)
-export const setToken = (token: string) => Cookies.set(tokenKey, token)
+export const setToken = (token: string) => Cookies.set(tokenKey, token, {
+  expires: 1
+})
 export const removeToken = () => Cookies.remove(tokenKey)
