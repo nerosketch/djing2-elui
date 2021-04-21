0b3945bc31d031829f6f73d7951df9ec3ff522c7
Dmitry Novikov <nerosketch@gmail.com>
2021-04-21 23:08:02 +0300

    click by push notification

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,5 @@
 // push
-self.addEventListener('push', function(event) {
+self.addEventListener('push', event => {
   const message = JSON.parse(event.data.text())
   event.waitUntil(
     self.registration.showNotification(message.title, {
@@ -8,6 +8,28 @@ self.addEventListener('push', function(event) {
       click_action: message.url
     })
   )
+})
+
+const defurl = '/customers'
+self.addEventListener('notificationclick', event => {
+  const target = event.notification.data.click_action || defurl
+  event.notification.close()
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(clientList => {
+    for (const i = 0; i < clientList.length; i++) {
+      const client = clientList[i];
+      if (client.url == defurl && 'focus' in client) {
+        return client.focus()
+      }
+    }
+    if (clients.openWindow) {
+      return clients.openWindow(defurl)
+    }
+  }))
 })
 
 workbox.core.setCacheNameDetails({ prefix: 'djing2-elui' })
