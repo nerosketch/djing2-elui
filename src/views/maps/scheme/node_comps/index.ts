a4285d7fc3bcdd04aa2f64d077acd08cd722cdf2
Dmitry Novikov <nerosketch@gmail.com>
2021-08-11 16:44:47 +0300

    removed unused

--- src/views/maps/scheme/node_comps/index.ts
+++ src/views/maps/scheme/node_comps/index.ts
@@ -1,14 +1,10 @@
 import Drawflow from 'drawflow'
 import VueClassContainer from './class-container'
-import Test from "./test.vue"
 import Router from "./router.vue"
-import Device from './device.vue'
 import SwitchNode from './switch.vue'
 
 const nodes: VueClassContainer[] = [
-  new VueClassContainer(Test, 'Тест'),
   new VueClassContainer(Router, 'Роутер', 2, 2),
-  new VueClassContainer(Device, 'Устройство', 1, 1, { text: '' }),
   new VueClassContainer(SwitchNode, 'Свич', 12, 12)
 ]
 
