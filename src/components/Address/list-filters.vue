654eebaefa5595f4e310210b1fa3c2df17213618
Dmitry Novikov <nerosketch@gmail.com>
2022-01-18 14:25:14 +0300

    new icon

--- src/components/Address/list-filters.vue
+++ src/components/Address/list-filters.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   el-form(inline)
     el-form-item(:label="$t('customers.streets')")
-      address-street-choice(:addrId="addrId", v-model="streetVal")
+      address-street-choice(:addrId="addrId" v-model="streetVal")
 
     el-form-item(:label="$t('route.groups')")
-      groups-choice(v-model="groupVal", :fetchFunction="fetchGroups")
+      groups-choice(v-model="groupVal" :fetchFunction="fetchGroups")
 </template>
 
 <script lang="ts">
