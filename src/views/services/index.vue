b3ab99d1e6c7b5c391c36529f4fc5e6c0900ddd2
Dmitry Novikov <nerosketch@gmail.com>
2022-07-26 12:47:32 +0300

    refactoring tabs

--- src/views/services/index.vue
+++ src/views/services/index.vue
@@ -1,38 +1,20 @@
 <template lang="pug">
-  .tab-container
-    el-tabs(
-      v-model="activeTabName"
-      type="border-card"
-    )
-      el-tab-pane(
-        :label="$t('tariffs')"
-        name="services"
-        lazy
-      )
-        keep-alive
-          service-list
+tabs(
+  :tabs="tabItems"
+  activeTabName="services"
+)
+  template(#services)
+    service-list
+  template(#periodicpays)
+    periodic-pay-list
+  template(#shots)
+    shot-list
 
-      el-tab-pane(
-        :label="$t('recurrentPayments')"
-        name="periodicpays"
-        lazy
-      )
-        keep-alive
-          periodic-pay-list
-
-      el-tab-pane(
-        :label="$t('singlePayments')"
-        name="shots"
-        lazy
-      )
-        keep-alive
-          shot-list
 </template>
 
 <script lang="ts">
-import { Component } from 'vue-property-decorator'
-import { mixins } from 'vue-class-component'
-import TabMixin from '@/utils/tab-mixin'
+import { Component, Vue } from 'vue-property-decorator'
+import Tabs, { ICustomTabItem } from '@/components/tabs/tabs.vue'
 import ServiceList from './services-list.vue'
 import ShotList from './shot-list.vue'
 import PeriodicPayList from './periodicpay-list.vue'
@@ -42,10 +24,15 @@ import PeriodicPayList from './periodicpay-list.vue'
   components: {
     ServiceList,
     ShotList,
+    Tabs,
     PeriodicPayList
   }
 })
-export default class extends mixins(TabMixin) {
-  protected activeTabName = 'services'
+export default class extends Vue {
+  private tabItems: ICustomTabItem[] = [
+    { name: 'services', title: this.$t('tariffs') },
+    { name: 'periodicpays', title: this.$t('recurrentPayments') },
+    { name: 'shots', title: this.$t('singlePayments') },
+  ]
 }
 </script>
