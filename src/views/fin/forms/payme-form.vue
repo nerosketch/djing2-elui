866aad85bea47867348f0bb4ea60c5ebd37dd6b3
Dmitry Novikov <nerosketch@gmail.com>
2022-08-11 12:02:44 +0300

    added form for PayMe

---
+++ src/views/fin/forms/payme-form.vue
@@ -0,0 +1,98 @@
+<template lang="pug">
+  el-form(
+    ref="form"
+    label-width="100px"
+    status-icon
+    :rules="frmRules"
+    :model="frmMod"
+    v-loading="isLoading")
+    el-form-item(:label="$t('title')" prop="title")
+      el-input(v-model="frmMod.title")
+
+    el-form-item(:label="$t('slug')" prop="slug")
+      el-input(v-model="frmMod.slug")
+
+    el-form-item
+      el-button(
+        icon="el-icon-upload"
+        type="primary"
+        @click="onSubmit"
+        :loading="isLoading"
+        :disabled="!$perms.fin_app.change_basepaymentmodel")
+        | {{ $t('save') }}
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { Form } from 'element-ui'
+import { IPayPaymeGateway } from '@/api/fin/types'
+import { addPaymePayGateway, changePaymePayGateway, getPaymePayGateway } from '@/api/fin/req'
+
+@Component({
+  name: 'PaymeGwForm'
+})
+export default class extends Vue {
+  private isLoading = false
+
+  private frmRules = {
+    title: [
+      { required: true, message: this.$tc('nameOfGroup'), trigger: 'blur' },
+      { max: 64, trigger: 'change' }
+    ],
+    slug: [
+      { required: true, message: this.$tc('iLlBeSure'), trigger: 'blur' },
+      { max: 32, message: this.$tc('only32Symbols'), trigger: 'change' }
+    ]
+  }
+
+  private changeGw(dat: IPayPaymeGateway) {
+    this.frmMod.title = dat.title
+    this.frmMod.slug = dat.slug
+  }
+
+  private frmMod = {
+    title: '',
+    slug: '',
+  }
+
+  get isNew() {
+    return this.$store.state.basepaymentmodel.id === 0
+  }
+
+  private async fetchGw() {
+    if (!this.isNew) {
+      const { data } = await getPaymePayGateway(this.$store.state.basepaymentmodel.id)
+      this.changeGw(data)
+    }
+  }
+
+  created() {
+    this.fetchGw()
+  }
+
+  private onSubmit() {
+    (this.$refs.form as Form).validate(async valid => {
+      if (valid) {
+        this.isLoading = true
+        let newDat
+        try {
+          if (this.isNew) {
+            newDat = await addPaymePayGateway(this.frmMod)
+          } else {
+            newDat = await changePaymePayGateway(
+              this.$store.state.basepaymentmodel.id,
+              this.frmMod
+            )
+          }
+          this.isLoading = false
+          this.$emit('done', newDat)
+        } catch {
+          this.isLoading = false
+        }
+      } else {
+        this.$message.error(this.$tc('fixFormErrs'))
+      }
+    })
+  }
+}
+</script>
